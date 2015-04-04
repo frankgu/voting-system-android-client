70ce5604e67f68cb836fa4f67ca6c5e0710084db
Dongfeng Gu <gdf1992803@hotmail.com>
2015-04-04 13:21:58 -0400

    finished the android client

--- Andoird-VotingSystem/src/com/dongfeng/andoird_votingsystem/Voting.java
+++ Andoird-VotingSystem/src/com/dongfeng/andoird_votingsystem/Voting.java
@@ -1,8 +1,10 @@
 package com.dongfeng.andoird_votingsystem;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -22,20 +24,21 @@ public class Voting extends ActionBarActivity {
 
 	private String username = null;
 	private RadioGroup radioGroup;
+	private HashMap<Integer, String> hm = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_voting);
-		
+
+		Intent i = getIntent();
+		username = i.getExtras().getString("username", "");
 		checkVoterVoteState();
 		showCandidateList();
 	}
 
 	private void checkVoterVoteState() {
 		// initialize the user name and check if the voter has voted or not
-		Intent i = getIntent();
-		username = i.getExtras().getString("username", "");
 		String reply_data = Transmission.getInstance()
 				.sendData("6:" + username);
 		if (reply_data.compareTo("null") == 0) {
@@ -47,11 +50,12 @@ public class Voting extends ActionBarActivity {
 		} else {
 			// check the voter state
 			String[] reply_datas = reply_data.split(":");
+			System.out.println(reply_data);
 			if (reply_datas[1].compareTo("1") == 0) {
 				// voter hasn't voted, do nothing
-			} else if (reply_datas[2].compareTo("2") == 0) {
+			} else if (reply_datas[1].compareTo("2") == 0) {
 				// voter already vote, show the candidate name
-				String candidateName = reply_datas[1];
+				String candidateName = reply_datas[2] + " " + reply_datas[3];
 				TextView candiate_view = (TextView) findViewById(R.id.voting_candidate_view);
 				candiate_view.setText(candidateName);
 				candiate_view.setTextColor(Color.GREEN);
@@ -64,6 +68,7 @@ public class Voting extends ActionBarActivity {
 		}
 	}
 
+	@SuppressLint("UseSparseArrays")
 	private void showCandidateList() {
 		// show the candidate list
 		String candidate_list = Transmission.getInstance().sendData("4:");
@@ -76,23 +81,19 @@ public class Voting extends ActionBarActivity {
 		} else {
 			// add the candidate to the radio group
 			String[] candidate_lists = candidate_list.split(":");
-			final List<String> candidate_name = new ArrayList<String>();
+			hm = new HashMap<Integer, String>();
+			radioGroup = (RadioGroup) findViewById(R.id.voting_radiogroup_candidatelist);
 			for (int x = 1; x < candidate_lists.length; x = x + 3) {
 				String candidateName = candidate_lists[x + 1] + " "
 						+ candidate_lists[x + 2];
-				candidate_name.add(candidateName);
-			}
-
-			radioGroup = (RadioGroup) findViewById(R.id.voting_radiogroup_candidatelist);
-			int count = 0;
-			for (String candidate : candidate_name) {
+				// store the id correspond with the candidate username
+				hm.put(x, candidate_lists[x] + ":" + candidateName);
+				// add the radio button to the radio group
 				RadioButton rdbtn = new RadioButton(this);
-				rdbtn.setId(count);
-				rdbtn.setText(candidate);
-				count++;
+				rdbtn.setId(x);
+				rdbtn.setText(candidateName);
 				radioGroup.addView(rdbtn);
 			}
-
 		}
 	}
 
@@ -118,12 +119,14 @@ public class Voting extends ActionBarActivity {
 	public void voting_button_click_event(View view) {
 
 		// get the candidate choice
-		String radiovalue = ((RadioButton) findViewById(radioGroup
-				.getCheckedRadioButtonId())).getText().toString();
+		RadioButton selectedRadioButton = (RadioButton) findViewById(radioGroup
+				.getCheckedRadioButtonId());
+		int radiobuttonID = selectedRadioButton.getId();
+		String candidateUsername = hm.get(radiobuttonID);
 
 		// vote for this candidate
 		String reply_data = Transmission.getInstance().sendData(
-				"2:" + username + ":" + radiovalue);
+				"2:" + username + ":" + candidateUsername);
 		if (reply_data.compareTo("null") == 0) {
 			// fail to connect to the server
 			MessageDialog.getInstance().showErrorMessage(this,
